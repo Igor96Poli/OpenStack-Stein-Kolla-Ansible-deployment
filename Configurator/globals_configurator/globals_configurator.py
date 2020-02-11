34d1ea0d7e3b5de33f503f5b0d1b7a1f3f3969bd
Andrea Della Chiesa <andr3a.dc@gmail.com>
2020-02-11 22:38:36 +0100

    Added Configurators
    Added configurators for globals.yml, multinode and interfaces files.

---
+++ Configurator/globals_configurator/globals_configurator.py
@@ -0,0 +1,28 @@
+fin = open("globals_template.yml", "rt")
+fout = open("globals.yml", "wt")
+
+base_distro = raw_input("Select your base distro [centos, debian, oraclelinux, rhel, ubuntu]: ")
+if (base_distro not in ['centos', 'debian', 'oraclelinux', 'rhel', 'ubuntu']):
+    base_distro = "ubuntu"
+
+
+install_type = raw_input("Select kolla installation time [binary, source]: ")
+if (install_type not in ['binary', 'source']):
+    install_type = "source"
+
+openstack_release = raw_input("Select your Openstack release: ")
+
+internal_ip = raw_input("Select the Internal IP Address: ")
+
+external_ip = raw_input("Select the External IP Address: ")
+
+for line in fin:
+    line = line.replace("var_base_distro", base_distro)
+    line = line.replace("var_install_type", install_type)
+    line = line.replace("var_openstack_release", openstack_release)
+    line = line.replace("var_external_ip", external_ip)
+    line = line.replace("var_internal_ip", internal_ip)
+    fout.write(line)
+	
+fin.close()
+fout.close()
---
+++ Configurator/interfaces_configurator/interfaces_configurator.py
@@ -0,0 +1,35 @@
+file = open('interfaces', 'w')
+
+#Loopback interface
+file.write("auto lo\niface lo inet loopback\n\n")
+
+#Setup primary interface
+primary_interface = raw_input("Insert your primary interface name: ")
+file.write("auto " + primary_interface + "\niface " + primary_interface + " inet static\n")
+
+ip_address = raw_input("Insert your primary interface IP address: ")
+file.write("  address " + ip_address + "\n")
+
+netmask = raw_input("Insert your primary interface netmask: ")
+file.write("  netmask " + netmask + "\n")
+
+dg_address = raw_input("Insert your Default Gateway IP address: ")
+file.write("  gateway " + dg_address + "\n")
+
+while True:
+    dns_address = raw_input("Insert your DNS IP address (q to quit): ")
+    if (dns_address == 'q'):
+        break
+    file.write("  dns-nameserver "+ dns_address + "\n")
+
+file.write('\n')
+
+
+#Setup secondary interface
+secondary_interface = raw_input("\nInsert your secondary interface name: ")
+file.write("auto " + secondary_interface + "\niface " + secondary_interface + " inet manual\n")
+file.write("up ip link set dev " + secondary_interface + "up\n")
+file.write("down ip link set dev " + secondary_interface + "down\n")
+
+#Close file
+file.close()
